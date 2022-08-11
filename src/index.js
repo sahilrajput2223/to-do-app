8e075e9fcb96cb15da300366020ce16bca5dc92d
Sahil Rajput <rajputsahil.2204@gmail.com>
2022-08-11 12:08:35 +0530

    Basic App Component Created And Added in View

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDom from "react-dom"
 
+import App from "./App"
 
-
-ReactDom.render("", document.getElementById("root"))
+ReactDom.render(<App />, document.getElementById("root"))
