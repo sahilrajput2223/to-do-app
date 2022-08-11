9cc4cb9e930f0b1b58345a10eb21b78fdd63b598
Sahil Rajput <rajputsahil.2204@gmail.com>
2022-08-11 15:11:06 +0530

    Header and background Design created - also font changed

--- src/MainSection/MainSection.js
+++ src/MainSection/MainSection.js
@@ -1,9 +1,19 @@
 import React from "react"
 
+import "./MainSection.css"
+
 const MainSection = () => {
     return (
         <React.Fragment>
-
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12 com-sm-12">
+                        <div className="mainSection">
+                            <h3>To Do Things</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </React.Fragment>
     );
 }
