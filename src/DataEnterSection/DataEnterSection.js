8c60baa6618ea24dbce98ebcf460cf4b77b74d0e
Sahil Rajput <rajputsahil.2204@gmail.com>
2022-08-12 20:00:22 +0530

    Basic Event Function and useState hook created

--- src/DataEnterSection/DataEnterSection.js
+++ src/DataEnterSection/DataEnterSection.js
@@ -3,13 +3,30 @@ import React from "react"
 import "./DataEntrySection.css"
 
 const DataEnterSection = () => {
+
+    const [task, setTask] = React.useState({ task: "" });
+    const [taskList, setTaskList] = React.useState([]);
+
+    const handleOnChangeEvent = () => {
+
+    }
+
+    const handleOnSubmitEvent = () => {
+
+    }
+
+    const handleOnResetEvent = () => {
+
+    }
+
     return (
         <React.Fragment>
             <div className="container">
-                <div className="dataSection">
-                    <input type="text" placeholder="e.g. take break" />
+                <form className="dataSection">
+                    <input type="text" id="todoTask" name="todoTask" placeholder="e.g. take break" />
                     <button>Submit</button>
-                </div>
+                </form>
+
             </div>
         </React.Fragment>
     );
